77a082a12354c6c23e1755382d38cdf1d8a36b06
polish <polishcivil@gmail.com>
2019-11-02 03:19:48 +0100

    Poison

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/npcs/combat/core/attack/hit/effect/posion/PoisonEffect.kt
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/npcs/combat/core/attack/hit/effect/posion/PoisonEffect.kt
@@ -1,12 +1,17 @@
 package gg.rsmod.plugins.content.npcs.combat.core.attack.hit.effect.posion
 
-import gg.rsmod.game.model.Hit
 import gg.rsmod.game.model.entity.Player
+import gg.rsmod.plugins.api.ext.message
+import gg.rsmod.plugins.content.mechanics.poison.poison
 import gg.rsmod.plugins.content.npcs.combat.configuration.attack.hit.effect.poison.PoisonEffectConfiguration
 import gg.rsmod.plugins.content.npcs.combat.core.attack.hit.effect.NPCHitEffect
 
 class PoisonEffect(private val cfg: PoisonEffectConfiguration) : NPCHitEffect {
     override fun apply(target: Player) {
-        println("Poisoning")
+        if (target.world.random(100) > this.cfg.chance) {
+            target.poison(this.cfg.damage) {
+                target.message("You have been poisoned")
+            }
+        }
     }
 }
