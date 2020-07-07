a6a9af757a5610f34e5385a964c3b0ed518840bc
Justin Conner <jstncnnr@gmail.com>
2020-07-07 10:00:13 -0700

    Add SetHintArrow packet encoder

--- game/src/main/kotlin/gg/rsmod/game/message/MessageEncoderSet.kt
+++ game/src/main/kotlin/gg/rsmod/game/message/MessageEncoderSet.kt
@@ -60,6 +60,7 @@ class MessageEncoderSet {
         put(OnDialogAbortEncoder(), TriggerOnDialogAbortMessage::class.java)
         put(UpdateRebootTimerEncoder(), UpdateRebootTimerMessage::class.java)
         put(MapAnimEncoder(), MapAnimMessage::class.java)
+        put(HintArrowEncoder(), SetHintArrowMessage::class.java)
     }
 
     private fun <T : Message> put(encoder: MessageEncoder<T>, message: Class<out T>) {
