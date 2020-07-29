c89c361e808e0d1c1eba91e48117733a469a96ef
chromesnow <stretchedmp3@gmail.com>
2020-07-29 18:11:07 -0500

    Revert "Merge pull request #3 from nbness2/enhancement/CyclableTask"
    This reverts commit 7ec1cf902a4dd9ea3409b6846ece52422183e6f7, reversing
    changes made to 3377d77cad1bf87d911fddd88a572781645639ad.

--- game/src/main/kotlin/gg/rsmod/game/model/queue/QueueTaskSet.kt
+++ game/src/main/kotlin/gg/rsmod/game/model/queue/QueueTaskSet.kt
@@ -1,26 +1,24 @@
 package gg.rsmod.game.model.queue
 
-import gg.rsmod.game.model.coroutine.cyclable.set.CyclableTaskSet
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import java.util.*
 import kotlin.coroutines.createCoroutine
 
 /**
- * A Re-implementation of [QueueTaskSet] as a [CyclableTaskSet]<[QueueTask]> to preserve functionality and backwards compatibility.
+ * A system responsible for task coroutine logic.
  *
- * @author Curtis Woodard <Nbness2@gmail.com>
+ * @author Tom <rspsmods@gmail.com>
  */
-abstract class QueueTaskSet: CyclableTaskSet<QueueTask> {
+abstract class QueueTaskSet {
 
-    override val queue: LinkedList<QueueTask> = LinkedList()
+    protected val queue: LinkedList<QueueTask> = LinkedList()
 
-    override fun queue(
-            ctx: Any,
-            dispatcher: CoroutineDispatcher,
-            priority: TaskPriority,
-            block: suspend QueueTask.(CoroutineScope) -> Unit
-    ) {
+    val size: Int get() = queue.size
+
+    abstract fun cycle()
+
+    fun queue(ctx: Any, dispatcher: CoroutineDispatcher, priority: TaskPriority, block: suspend QueueTask.(CoroutineScope) -> Unit) {
         val task = QueueTask(ctx, priority)
         val suspendBlock = suspend { block(task, CoroutineScope(dispatcher)) }
 
@@ -31,5 +29,26 @@ abstract class QueueTaskSet: CyclableTaskSet<QueueTask> {
         }
 
         queue.addFirst(task)
+    }
+
+    /**
+     * In-game events sometimes must return a value to a plugin. An example are
+     * dialogs which must return values such as input, button click, etc.
+     *
+     * @param value
+     * The return value that the plugin has asked for.
+     */
+    fun submitReturnValue(value: Any) {
+        val task = queue.peek() ?: return // Shouldn't call this method without a queued task.
+        task.requestReturnValue = value
+    }
+
+    /**
+     * Remove all [QueueTask] from our [queue], invoking each task's [QueueTask.terminate]
+     * before-hand.
+     */
+    fun terminateTasks() {
+        queue.forEach { it.terminate() }
+        queue.clear()
     }
 }
